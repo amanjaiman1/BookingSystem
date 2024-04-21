44ba93de690e39a650ce710a4c6a3ccb7b3c5e2c
adityaraj14-12 <adityapersonalac@gmail.com>
2024-04-21 14:30:35 +0530

    database

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 //PLEASE EDIT YOUR MONGO DB CONNECTION HERE
 /*YOU CAN FIND SAMPLE COLLECTION at mongodb_collections folder */
 // var mongoURL = "mongodb://127.0.0.1:27017/bookmystay";
-var mongoURL = "mongodb://localhost:27017/book";
+var mongoURL = "mongodb+srv://adityapersonalac:adityaraj@cluster0.jxlfhay.mongodb.net/event";
 
 mongoose.connect(mongoURL, { useUnifiedTopology: true, useNewUrlParser: true });
 
